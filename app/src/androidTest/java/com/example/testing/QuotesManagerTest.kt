c1b6fb99c05ad9588e5d8801ee4c933ba42e76a3
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-07-17 23:46:14 +0530

    first nonUI test implemented

--- app/src/androidTest/java/com/example/testing/QuotesManagerTest.kt
+++ app/src/androidTest/java/com/example/testing/QuotesManagerTest.kt
@@ -1,13 +1,18 @@
 package com.example.testing
 
-import org.junit.Assert.*
-
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
 import org.junit.Test
 
 class QuotesManagerTest {
 
     @Test
     fun populateQuoteFromAssets() {
+        //arrange
+        val quoteManager = QuotesManager()
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        //act
+        quoteManager.populateQuoteFromAssets(context, "")
 
     }
 }
