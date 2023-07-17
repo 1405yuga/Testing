4f45827c93a1480645fd5c4d02db93ba481ba35a
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-07-17 23:28:56 +0530

    created function to quotes list

--- app/src/main/java/com/example/testing/QuotesManager.kt
+++ app/src/main/java/com/example/testing/QuotesManager.kt
@@ -1,4 +1,23 @@
 package com.example.testing
 
-class QuotesManager {
+import android.content.Context
+import com.google.gson.Gson
+
+class QuotesManager() {
+
+    var quoteList = emptyArray<Quote>()
+    val currentQuoteIndex = 0
+
+    fun populateQuoteFromAssets(context: Context, filename: String) {
+        val inputStream = context.assets.open(filename)
+        val size: Int = inputStream.available()
+        val buffer = ByteArray(size)
+        inputStream.read(buffer)
+        inputStream.close()
+
+        val json = String(buffer, Charsets.UTF_8)
+        val gson = Gson()
+        quoteList = gson.fromJson(json,Array<Quote>::class.java)
+    }
+
 }
