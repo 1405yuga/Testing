0953617ac85ecf67fea974fa512daeef371609b4
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-07-17 22:37:58 +0530

    created companion object

--- app/src/test/java/com/example/testing/utils/ParameterizedExample.kt
+++ app/src/test/java/com/example/testing/utils/ParameterizedExample.kt
@@ -2,7 +2,10 @@ package com.example.testing.utils
 
 import org.junit.Assert
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
 
+@RunWith(value = Parameterized::class)
 class ParameterizedExample(val input : String , val expectedResult : Boolean) {
 
     @Test
@@ -14,6 +17,18 @@ class ParameterizedExample(val input : String , val expectedResult : Boolean) {
 
         //assert
         Assert.assertEquals(expectedResult, result)
+    }
+
+    companion object{
+
+        @JvmStatic
+        @Parameterized.Parameters
+        fun data() : List<Array<Any>>{
+            return listOf(
+                arrayOf("hee",false),
+                arrayOf("lol",true)
+            )
+        }
     }
 
 }
