a4d5e27feef91bc426c906ec5f686922d429c214
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-07-17 23:48:51 +0530

    first nonUI test implemented handled assert

--- app/src/androidTest/java/com/example/testing/QuotesManagerTest.kt
+++ app/src/androidTest/java/com/example/testing/QuotesManagerTest.kt
@@ -3,10 +3,11 @@ package com.example.testing
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
 import org.junit.Test
+import java.io.FileNotFoundException
 
 class QuotesManagerTest {
 
-    @Test
+    @Test(expected = FileNotFoundException::class) //assert
     fun populateQuoteFromAssets() {
         //arrange
         val quoteManager = QuotesManager()
