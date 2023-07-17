8ee8a77a56e73f42195ad52ed44b6408b6252ec9
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-07-17 22:15:57 +0530

    first test implemented

--- app/src/test/java/com/example/testing/utils/HelperTest.kt
+++ app/src/test/java/com/example/testing/utils/HelperTest.kt
@@ -8,5 +8,13 @@ class HelperTest {
 
     @Test
     fun isPalindrome() {
+        //arrange
+        val helper = Helper()
+
+        //act
+        val result = helper.isPalindrome("Hello")
+
+        //assert
+        assertEquals(false,result)
     }
 }
